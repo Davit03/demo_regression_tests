8df8dcb8f35854388b0852e6e64fd8e1a57c862b
Davit <57663927+DavitV-yan@users.noreply.github.com>
2022-01-10 09:56:53 +0400

    Update BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,7 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
+//
 import static com.codeborne.selenide.Selenide.*;
 
 public class BaseTest {
